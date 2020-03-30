a58ab2ea9411a0e6cc509e046cbc5928fa8c45f3
Kapil Garg <kapil.garg@uber.com>
2020-03-30 10:47:01 +0530

    Revert "Gif Support"
    This reverts commit 4d5f2d1e50b28708e5c7983b1b459546d17fcf5c.

--- vips.go
+++ vips.go
@@ -21,7 +21,6 @@ const DEBUG = false
 var (
 	MARKER_JPEG = []byte{0xff, 0xd8}
 	MARKER_PNG  = []byte{0x89, 0x50}
-	MARKER_GIF  = []byte{0x47, 0x49, 0x46}
 	MARKER_WEBP = []byte{0x57, 0x45, 0x42, 0x50}
 )
 
@@ -31,7 +30,6 @@ const (
 	UNKNOWN ImageType = iota
 	JPEG
 	PNG
-	GIF
 	WEBP
 )
 
@@ -121,8 +119,6 @@ func Resize(buf []byte, o Options) ([]byte, error) {
 		typ = JPEG
 	case bytes.Equal(buf[:2], MARKER_PNG):
 		typ = PNG
-	case bytes.Equal(buf[:3], MARKER_GIF):
-		typ = GIF
 	case bytes.Equal(buf[8:12], MARKER_WEBP):
 		typ = WEBP
 	default:
@@ -138,8 +134,6 @@ func Resize(buf []byte, o Options) ([]byte, error) {
 		C.vips_jpegload_buffer_seq(unsafe.Pointer(&buf[0]), C.size_t(len(buf)), &image)
 	case PNG:
 		C.vips_pngload_buffer_seq(unsafe.Pointer(&buf[0]), C.size_t(len(buf)), &image)
-	case GIF:
-		C.vips_gifload_buffer_seq(unsafe.Pointer(&buf[0]), C.size_t(len(buf)), &image)
 	case WEBP:
 		C.vips_webpload_buffer_seq(unsafe.Pointer(&buf[0]), C.size_t(len(buf)), &image)
 	}
